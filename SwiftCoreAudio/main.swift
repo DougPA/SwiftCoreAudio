ceb0d5ea31707818b45e921cc9ccd350d51adf3f
Douglas Adams <doug@k3tzr.net>
2016-07-02 20:55:46 -0400

    Initial Commit

---
+++ SwiftCoreAudio/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftCoreAudio
+//
+//  Created by Douglas Adams on 7/2/16.
+//  Copyright © 2016 Douglas Adams. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
